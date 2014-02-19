f429aedf5e6de3047e0ee91aef37cecf18ab8ac3
Bilal Quadri <bilalquadri92@gmail.com>
2014-02-19 01:20:54 -0800

    Add dependencies & configure tests

---
+++ test/setup.js
@@ -0,0 +1,34 @@
+// Test helpers
+global.sinon = require('sinon');
+global.chai = require('chai');
+global.should = require('chai').should();
+global.expect = require('chai').expect;
+global.AssertionError = require('chai').AssertionError;
+chai.use(require('sinon-chai'));
+chai.use(require('chai-fs'));
+
+// Polyfills
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function (searchString, position) {
+      position = position || 0;
+      return this.indexOf(searchString, position) === position;
+    }
+  });
+}
+if (!String.prototype.endsWith) {
+  Object.defineProperty(String.prototype, 'endsWith', {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function (searchString, position) {
+      position = position || this.length;
+      position = position - searchString.length;
+      var lastIndex = this.lastIndexOf(searchString);
+      return lastIndex !== -1 && lastIndex === position;
+    }
+  });
+}
