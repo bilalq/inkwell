e65dd1bb6ba87e88d32968605ebddf140986ec7f
Bilal Quadri <bilalquadri92@gmail.com>
2014-03-20 21:25:10 -0700

    Fix style inconsistencies

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,11 @@
 // Test helpers
-global.sinon = require('sinon');
-global.chai = require('chai');
-global.should = require('chai').should();
-global.expect = require('chai').expect;
-global.AssertionError = require('chai').AssertionError;
-chai.use(require('sinon-chai'));
-chai.use(require('chai-fs'));
+global.sinon = require('sinon')
+global.chai = require('chai')
+global.should = require('chai').should()
+global.expect = require('chai').expect
+global.AssertionError = require('chai').AssertionError
+global.chai.use(require('sinon-chai'))
+global.chai.use(require('chai-fs'))
 
 // Polyfills
 if (!String.prototype.startsWith) {
@@ -14,10 +14,10 @@ if (!String.prototype.startsWith) {
     configurable: false,
     writable: false,
     value: function (searchString, position) {
-      position = position || 0;
-      return this.indexOf(searchString, position) === position;
+      position = position || 0
+      return this.indexOf(searchString, position) === position
     }
-  });
+  })
 }
 if (!String.prototype.endsWith) {
   Object.defineProperty(String.prototype, 'endsWith', {
@@ -25,10 +25,10 @@ if (!String.prototype.endsWith) {
     configurable: false,
     writable: false,
     value: function (searchString, position) {
-      position = position || this.length;
-      position = position - searchString.length;
-      var lastIndex = this.lastIndexOf(searchString);
-      return lastIndex !== -1 && lastIndex === position;
+      position = position || this.length
+      position = position - searchString.length
+      var lastIndex = this.lastIndexOf(searchString)
+      return lastIndex !== -1 && lastIndex === position
     }
-  });
+  })
 }
